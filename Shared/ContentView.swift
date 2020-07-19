6a2affe6d6538c2d5b28f307f5e011f28a5d2ca5
Bastian Inuk Christensen <bastian@inuk.blog>
2020-07-19 14:46:58 +0200

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Bastian Inuk Christensen on 19/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Manga_EpubApp.swift
@@ -0,0 +1,17 @@
+//
+//  Manga_EpubApp.swift
+//  Shared
+//
+//  Created by Bastian Inuk Christensen on 19/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct Manga_EpubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
